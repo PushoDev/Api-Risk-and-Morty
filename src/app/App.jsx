f6878d6d794426e30cdea4ff95ada861aa12f3f4
pushodev <bethocubans1990@gmail.com>
2024-09-30 15:42:18 -0400

    Personalizar Navbar

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,13 +1,11 @@
+// Componentes
+import Navbar from '../components/Navbar'
 function App() {
 
 
   return (
     <>
-      <div>
-        <h1 className="text-3xl font-bold bg-red-500">
-          Hola desde App.jsx
-        </h1>
-      </div>
+      <Navbar />
 
     </>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,25 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  daisyui: {
+    themes: ["synthwave", "retro"],
+  },
   theme: {
     extend: {
-
+      fontFamily: {
+        sans: ["Poppins"],
+      },
+      container: {
+        center: true,
+        padding: {
+          default: "1rem",
+          sm: "2rem",
+        },
+      },
     }
   },
   plugins: [
-
+    require('daisyui'),
   ],
 }
 
