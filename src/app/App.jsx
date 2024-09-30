81b754ca971456d55247b1719d9b631353ddb074
pushodev <bethocubans1990@gmail.com>
2024-09-30 13:29:57 -0400

    Init Projects

---
+++ src/app/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <h1 className="text-3xl font-bold bg-red-500">
+          Hola desde App.jsx
+        </h1>
+      </div>
+
+    </>
+  )
+}
+
+export default App
